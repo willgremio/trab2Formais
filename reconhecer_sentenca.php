05b1d5dd1f3f5fac82b320a81d0d8ee26c983e30
Willian Serafini <willian@onehost.com.br>
2015-12-07 11:33:59 -0200

    fazendo tratamento de varios estados estados destinos

--- reconhecer_sentenca.php
+++ reconhecer_sentenca.php
@@ -6,15 +6,29 @@ $sentenca = $_POST['data']['sentenca'];
 $DadosValorTerminalEstado = json_decode($_POST['data']['DadosValorTerminalEstado']);
 $SimbolosFinais = json_decode($_POST['data']['SimbolosFinais']);
 $SimboloInicial = $_POST['data']['SimboloInicial'];
-
-$ultimaLetra = strlen($sentenca) - 1;
+//var_dump($DadosValorTerminalEstado);exit;
+$indexUltimaLetra = strlen($sentenca) - 1;
 
 $estadoAtual = $SimboloInicial;
 $retorno['msg'] = 'Sentença não Reconhecida!';
 for ($i = 0; $i < strlen($sentenca); $i++) {
     $letra = $sentenca[$i];
-    //var_dump($letra);    
-    $estadoDestino = $DadosValorTerminalEstado->$letra->$estadoAtual;
+    //var_dump($DadosValorTerminalEstado->$letra->$estadoAtual); exit;   
+    if(Funcoes::temMaisDeUmEstadoDestino($DadosValorTerminalEstado->$letra->$estadoAtual)) {
+        $proximaLetra = '';
+        $nextIndex = $i + 1;
+        if(isset($sentenca[$nextIndex])) {
+            $proximaLetra = $sentenca[$nextIndex];
+        }
+        
+        //var_dump($proximaLetra);
+        //var_dump($sentenca[$nextIndex]);        
+        $estadoDestino = Funcoes::getEstadoDestino($DadosValorTerminalEstado->$letra->$estadoAtual, $letra, $proximaLetra, $estadoAtual);
+    } else {
+        $estadoDestino = $DadosValorTerminalEstado->$letra->$estadoAtual;
+    }
+    
+    
     //var_dump('estadoAtual: ' . $estadoAtual);
     //var_dump('estadoDestino: ' . $estadoDestino);
     if (empty($estadoDestino)) {
@@ -23,7 +37,7 @@ for ($i = 0; $i < strlen($sentenca); $i++) {
     }
 
     $estadoAtual = $estadoDestino;
-    if ($i == $ultimaLetra) {
+    if ($i == $indexUltimaLetra) {
         if (!Funcoes::estadoIsFinal($estadoAtual, $SimbolosFinais)) {
             echo json_encode($retorno);
             exit();
